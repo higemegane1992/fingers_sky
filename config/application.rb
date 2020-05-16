0050c901086373f0dd37f1c8c4887163051148c0
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-05-16 21:50:10 +0900

    Set up bootstrap and fontaweson

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,14 @@ module FingersSky
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
+
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
