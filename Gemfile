05c45eb51d556cadb8d4beb95d5e32e18b3a29db
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-05-16 22:35:51 +0900

    Set up react

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'dotenv-rails'
 gem 'public_suffix', '>= 4.0.5'
+gem 'react-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,7 @@ require("channels")
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import '../stylesheets/application.scss'
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
