90986d6c760d685b73b3ed4f0f35bdb02f362549
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-05-18 00:45:22 +0900

    Add columns to user  and fix controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,28 +3,28 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
+  protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(
-  #     :sign_up,
-  #     keys: [
-  #       :last_name,
-  #       :first_name,
-  #       :site_title,
-  #       :site_address
-  #     ]
-  #   )
-  #   devise_parameter_sanitizer.permit(
-  #     :account_update,
-  #     keys: [
-  #       :last_name,
-  #       :first_name,
-  #       :site_title,
-  #       :site_address
-  #     ]
-  #   )
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [
+        :last_name,
+        :first_name,
+        :site_title,
+        :site_address
+      ]
+    )
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [
+        :last_name,
+        :first_name,
+        :site_title,
+        :site_address
+      ]
+    )
+  end
 end
