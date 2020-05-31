da4d08638d039302e572919d8933ee52cb03b5fb
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-05-31 17:13:10 +0900

    Add user name link to events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @user_name = user.user_name
-    @events = user.events.order('id DESC').page(params[:page]).per(30)
+    @user = User.find(params[:id])
+    @events = @user.events.order('id DESC').page(params[:page]).per(30)
   end
 end
