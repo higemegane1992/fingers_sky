3ad638cd32aa5fcafec436545285963e57403841
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-06-06 15:09:32 +0900

    Add events and participants to user_path view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @events = @user.events.order('id DESC').page(params[:page]).per(30)
+    @events = @user.events.order('id DESC').page(params[:page]).per(12)
+    @participants = @user.participants.order('id DESC').page(params[:page]).per(12)
   end
 end
