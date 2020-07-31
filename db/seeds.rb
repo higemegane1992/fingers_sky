f6cc685e2581ba6493f2f48a93f8e2bd72d33842
higemegane1992 <kazuaki.hirata.0905@gmail.com>
2020-07-31 23:56:36 +0900

    Add db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,281 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  [
+    '筒井', '里桜', 'Tsutsui.Rio@fingers-sky.com', '5YOlBIC4', '5YOlBIC4', 'Tsutsui.Rio'
+  ],
+  [
+    '阿久津', '栄美', 'Akutsu.Emi@fingers-sky.com', 'ZHKPTVRf', 'ZHKPTVRf', 'Akutsu.Emi'
+  ],
+  [
+    '吉井', '詩温', 'Yoshii.Shion@fingers-sky.com', '5Xeu9mpD', '5Xeu9mpD', 'Yoshii.Shion'
+  ],
+  [
+    '新田', '沙希', 'Nitta.Saki@fingers-sky.com', 'dFMTqUz0', 'dFMTqUz0', 'Nitta.Saki'
+  ],
+  [
+    '三木', '康之', 'Miki.Yasuyuki@fingers-sky.com', 'gTDML4Oq', 'gTDML4Oq', 'Miki.Yasuyuki'
+  ],
+  [
+    '伊藤', '星桜', 'Itou.Seira@fingers-sky.com', '5ftNY8qu', '5ftNY8qu', 'Itou.Seira'
+  ],
+  [
+    '村田', '空澄', 'Murata.Asumi@fingers-sky.com', 'cgZ33YN6', 'cgZ33YN6', 'Murata.Asumi'
+  ],
+  [
+    '藤沢', '金吾', 'Fujisawa.Kingo@fingers-sky.com', 'x3tzOVfp', 'x3tzOVfp', 'Fujisawa.Kingo'
+  ],
+  [
+    '板倉', '椛', 'Itakura.Momiji@fingers-sky.com', 'fst1CBph', 'fst1CBph', 'Itakura.Momiji'
+  ],
+  [
+    '宮川', '真治', 'Miyakawa.Shinji@fingers-sky.com', 'hH7NTGBk', 'hH7NTGBk', 'Miyakawa.Shinji'
+  ],
+  [
+    '宇都宮', '徹', 'Utsunomiya.Tooru@fingers-sky.com', 'zBIN25bg', 'zBIN25bg', 'Utsunomiya.Tooru'
+  ],
+  [
+    '村木', '希望', 'Muraki.Nozomi@fingers-sky.com', 'WGKUFRwI', 'WGKUFRwI', 'Muraki.Nozomi'
+  ],
+  [
+    '成田', '俊光', 'Narita.Toshimitsu@fingers-sky.com', 'fxlDEfAx', 'fxlDEfAx', 'Narita.Toshimitsu'
+  ],
+  [
+    '前島', '圭子', 'Maejima.Keiko@fingers-sky.com', 'fS55WmGO', 'fS55WmGO', 'Maejima.Keiko'
+  ],
+  [
+    '竹下', '和香', 'Takeshita.Waka@fingers-sky.com', 'e3ZCvST4', 'e3ZCvST4', 'Takeshita.Waka'
+  ],
+  [
+    '勝又', '里桜', 'Katsumata.Rio@fingers-sky.com', 'rUpBGGkC', 'rUpBGGkC', 'Katsumata.Rio'
+  ],
+  [
+    '永島', '加奈子', 'Nagashima.Kanako@fingers-sky.com', 'A32IdjlW', 'A32IdjlW', 'Nagashima.Kanako'
+  ],
+  [
+    '花田', '葉月', 'Hanada.Hazuki@fingers-sky.com', 'zmOgWIhL', 'zmOgWIhL', 'Hanada.Hazuki'
+  ],
+  [
+    '新田', '優依', 'Nitta.Yui@fingers-sky.com', '1svQTA0Q', '1svQTA0Q', 'Nitta.Yui'
+  ],
+  [
+    '河合', '澄子', 'Kawai.Sumiko@fingers-sky.com', 'V3DWas2T', 'V3DWas2T', 'Kawai.Sumiko'
+  ],
+  [
+    '八木', '佐登子', 'Yagi.c@fingers-sky.com', 'cXL7sO0n', 'cXL7sO0n', 'Yagi.Yagi'
+  ],
+  [
+    '福岡', '博', 'Fukuoka.Hiroshi@fingers-sky.com', 'p7uR7xRy', 'p7uR7xRy', 'Fukuoka.Hiroshi'
+  ],
+  [
+    '中根', '宏美', 'Nakane.Hiromi@fingers-sky.com', 'aRDBNOWt', 'aRDBNOWt', 'Nakane.Hiromi'
+  ],
+  [
+    '寺沢', '奈々', 'Terasawa.Nana@fingers-sky.com', '0cXPNyBt', '0cXPNyBt', 'Terasawa.Nana'
+  ],
+  [
+    '手塚', '勇蔵', 'Tezuka.Yuuzou@fingers-sky.com', 'fAl4vszE', 'fAl4vszE', 'Tezuka.Yuuzou'
+  ],
+  [
+    '安斎', '光男', 'Anzai.Mitsuo@fingers-sky.com', 'IKQukNZn', 'IKQukNZn', 'Anzai.Mitsuo'
+  ],
+  [
+    '鳴海', '春香', 'Narumi.Haruka@fingers-sky.com', 'PhNmAEL3', 'PhNmAEL3', 'Narumi.Haruka'
+  ],
+  [
+    '島津', '麻友', 'Shimazu.Mayu@fingers-sky.com', 'w1W4PqVl', 'w1W4PqVl', 'Shimazu.Mayu'
+  ],
+  [
+    '長谷川', '虎雄', 'Hasegawa.Torao@fingers-sky.com', 'udWcRhmK', 'udWcRhmK', 'Hasegawa.Torao'
+  ],
+  [
+    '岩佐', '喜美子', 'Iwasa.Kimiko@fingers-sky.com', 'sIA7DuON', 'sIA7DuON', 'Iwasa.Kimiko'
+  ],
+  [
+    '安川', '武男', 'Yasukawa.Takeo@fingers-sky.com', '35LVfRdy', '35LVfRdy', 'Yasukawa.Takeo'
+  ],
+  [
+    '富沢', '治志', 'Tomizawa.Harushi@fingers-sky.com', 'xIkeVcLI', 'xIkeVcLI', 'Tomizawa.Harushi'
+  ],
+  [
+    '大津', '正司', 'Ootsu.Masashi@fingers-sky.com', 'MYAhV3SL', 'MYAhV3SL', 'Ootsu.Masashi'
+  ],
+  [
+    '谷口', '栞', 'Taniguchi.Shiori@fingers-sky.com', 'NmAL7uZs', 'NmAL7uZs', 'Taniguchi.Shiori'
+  ],
+  [
+    '佐竹', '梨花', 'Satake.Rika@fingers-sky.com', 'SyfmlBP2', 'SyfmlBP2', 'Satake.Rika'
+  ],
+  [
+    '杉村', '明菜', 'Sugimura.Akina@fingers-sky.com', 'z481d4MH', 'z481d4MH', 'Sugimura.Akina'
+  ]
+]
+
+users.each do |last_name, first_name, email, password, password_confirmation, user_name|
+  user = User.new(
+    {
+      last_name:             last_name,
+      first_name:            first_name,
+      email:                 email,
+      password:              password,
+      password_confirmation: password_confirmation,
+      user_name:             user_name
+    }
+  )
+  user.save!
+end
+
+events = [
+    ['英会話レッスン',
+      'シャイな日本人の方でも自信をもって英語を話せるよう追求した結果、オンライン教材で基礎を学び、ネイティブ講師から生きた英語を習う、というステップ式の学習法を開発しました。
+      ネイティブならではの正しい発音やアクセントに触れながら「本物の英語」を学ぶことで、英語を話す力が身に付きます。',
+      '2020-08-31 22:00:00.000000',
+      'ご自宅',
+      '',
+      1
+    ],
+    ['アクセサリー作り体験',
+      '金属の板から作り出す、オリジナルのシルバーアクセ！
+      初心者の方でも大丈夫、お店でオーダージュエリーを制作しているクラフトマンが丁寧にお教えします。',
+      '2020-09-05 13:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      2
+    ],
+    ['無料フットサル教室',
+      '無料体験会が行われます！！
+      「スクールの雰囲気ってどんな感じなんだろう？」
+      「どういう練習をするんだろう？」という声にお応えして体験会をさせて頂きます！
+      是非、スクールの雰囲気を肌で感じてみてください(*^-^*)',
+      '2020-09-05 10:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      3
+    ],
+    ['オンラインプログラミング勉強会',
+      'zoomというアプリを使用し、オンラインで開催しております！ もしインストールされていないようでしたら、 当日までにインストールしていただきますようお願いいたします。
+      勉強会開始数分前にzoomのURLをこちらの説明欄とメッセージにてお送りいたしますので、 時間になりましたらそのURLよりご参加ください！',
+      '2020-09-01 20:00:00.000000',
+      'ご自宅',
+      '',
+      4
+    ],
+    ['そば打ち体験',
+      '『蕎麦(そば)打ち体験』の魅力は何と言ってもその作業工程の楽しさ、そして自分の手で作った絶品の蕎麦を実際に食べられることにあります♪
+      普段、ご自宅などではなかなか準備することのできない本格的な材料(そば粉)や調理器具(石臼、のし板、麺棒、こね鉢、麺切り包丁など)を使用し、プロの技を間近で感じながら作る一杯はきっとどんな名店よりも美味しく感じられるハズです◎',
+      '2020-09-06 10:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      5
+    ],
+    ['ボクシングジム体験',
+      'キックボクシングプランを開催しており、初めての方でも体験出来ます！ミットにパンチやキックを打ち込んで、ストレス解消。親子向けのプランもご用意しております。',
+      '2020-09-06 15:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      6
+    ],
+    ['料理教室体験',
+      '料理教室のワークショップ体験の人気体験ランキングです。様々な料理のレシピを教わる、味噌などの調味料も作れる1日料理教室です！さまざまな技術や知識を学べます。
+      お一人様からファミリーまで楽しめます。',
+      '2020-09-06 17:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      7
+    ],
+    ['陶芸体験',
+      '陶芸で思い出作りをしませんか？
+      延べ100万人が参加して下さった水茎焼の陶芸教室はインストラクターがお手伝いをさせて頂くので初心者の方も安心です！',
+      '2020-09-05 17:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      8
+    ],
+    ['オンラインヨガ体験',
+      'zoomでヨガのレッスン動画を配信しているサービスです。
+      「ヨガに興味はあるけど定期的にスタジオに通えない」といったヨガ初心者の皆様だけでなく、
+      「もっと深くヨガを学びたい」という経験者の皆様にもご利用いただいております。',
+      '2020-09-02 18:00:00.000000',
+      'ご自宅',
+      '',
+      9
+    ],
+    ['ピアノ教室',
+      '個別レッスンとメンバー全員によるアンサンブルで構成されているのが特徴です。個別レッスンでは講師が一人ずつをサポート。
+      個人の課題や弾きたい曲を講師が丁寧にレッスンします。
+      アンサンブルでは全員で同じ曲を弾くことでリズム感を養うことができ、他人の演奏を聴くことで個人で弾くときも豊かな表現ができるようになります。',
+      '2020-09-03 18:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      10
+    ],
+    ['ギター教室',
+      '弊社ギター教室では現場で活躍するプロがあなたの専属講師につきます。レッスンだけで食べているような、いわゆるレッスンプロとは一線を画すプレミアムなレッスンをご提供します。
+      講師のスケジュールの空きさえあれば、曜日を問わずにいつでもレッスンの受講が可能です。一流プロの質の高いレッスンを好きな時に好きなだけ受講しましょう。',
+      '2020-09-04 18:00:00.000000',
+      '東京都〇〇区××',
+      '',
+      11
+    ],
+    ['無料オンラインダンスレッスン',
+      '家からトップクラスのダンサーのレッスンをzoomで受けられる企画を開催します。
+      自宅待機をしている人がダンスの力で少しでも前向きになればと考え、誰でも受けられるダンスレッスンをお届けします。',
+      '2020-09-04 18:00:00.000000',
+      'ご自宅',
+      '',
+      12
+    ]
+]
+
+i = 0
+
+while i < 9
+  events.each do |title, description, date, address, image, user_id|
+    Event.create!(
+      {
+        title:       title,
+        description: description,
+        date:        date.in_time_zone.since(i.week),
+        address:     address,
+        image:       image,
+        user_id:     user_id
+      }
+    )
+  end
+  i += 1
+end
+
+i = 1
+j = 13
+
+while i < 109
+  while j < 37
+    Participant.create!(
+      {
+        event_id: i,
+        user_id: j
+      }
+    )
+    j += 1
+  end
+  i += 1
+  j = 13
+end
+
+i = 0
+j = 1
+while i < 9
+  events.each do |title, description, date, address, image, user_id|
+    Message.create!(
+      {
+        subject:  "#{title}へのお申込み完了のお知らせ",
+        body:     "この度は#{title}にお申込みいただき誠にありがとうございます。
+                  #{description}
+                  日時：#{date.in_time_zone.strftime("%Y/%m/%d")}
+                  場所：#{address}",
+        event_id: j,
+        user_id:  user_id
+      }
+    )
+    j += 1
+  end
+  i += 1
+end
